25e5e1cb4cdd19bcf448731e3f190cc68f111269
Facundo Chavez <101282065+facukun@users.noreply.github.com>
2024-03-16 00:37:19 -0300

    v1.0.3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,22 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head>
-        <title>Gradient Border CSS & Tailwind</title>
-        <meta
-          name='description'
-          content='App to generate gradient borders with CSS and Tailwind'
-        />
-        <link rel='icon' href='/favicon.svg' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta property='og:image' content='/favicon.svg' />
-        <meta property='og:title' content='Gradient Border CSS & Tailwind' />
-        <meta
-          property='og:description'
-          content='App to generate gradient borders with CSS and Tailwind'
-        />
-        <meta name='google' content='notranslate' />
-      </Head>
+      <Head />
       <body className='h-screen w-screen bg-white'>
         <Analytics />
         <div className='absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#dfe1e5_1px,transparent_1px)] [background-size:16px_16px]'></div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,28 +7,44 @@ import BorderSettings from '@/layouts/BorderSettings/BorderSettings';
 import CodeBox from '@/layouts/CodeBox/CodeBox';
 import Footer from '@/layouts/Footer/Footer';
 import TestBox from '@/layouts/TestBox/TestBox';
-
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Gradient Border CSS & Tailwind</title>
+        <meta
+          name='description'
+          content='App to generate gradient borders with CSS and Tailwind'
+        />
+        <link rel='icon' href='/favicon.svg' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property='og:image' content='/favicon.svg' />
+        <meta property='og:title' content='Gradient Border CSS & Tailwind' />
+        <meta
+          property='og:description'
+          content='App to generate gradient borders with CSS and Tailwind'
+        />
+        <meta name='google' content='notranslate' />
+      </Head>
       <Header />
       <main className='w-full max-w-[1000px] flex flex-col mb-8 gap-5 md:gap-6 '>
-      <section className='w-full max-w-[1000px] flex flex-col gap-4 md:gap-6 md:flex-row'>
-        <main className='w-full h-[250px] mt-[5px] md:h-[unset] md:min-h-full'>
-          <PreviewBox />
-        </main>
-        <aside className='w-full flex flex-col gap-3 md:gap-3 md:min-w-[380px]'>
-          <ColorsPicker />
-          <footer className='w-full flex flex-row-reverse gap-5 md:gap-6 md:flex-row'>
-            <BorderSettings />
-            <SamplesBox />
-          </footer>
-        </aside>
-      </section>
-      <section className='w-full max-w-[1000px] flex flex-col gap-4 md:gap-6 md:pr-[5px]'>
-        <CodeBox />
-      </section>
+        <section className='w-full max-w-[1000px] flex flex-col gap-4 md:gap-6 md:flex-row'>
+          <main className='w-full h-[250px] mt-[5px] md:h-[unset] md:min-h-full'>
+            <PreviewBox />
+          </main>
+          <aside className='w-full flex flex-col gap-3 md:gap-3 md:min-w-[380px]'>
+            <ColorsPicker />
+            <footer className='w-full flex flex-row-reverse gap-5 md:gap-6 md:flex-row'>
+              <BorderSettings />
+              <SamplesBox />
+            </footer>
+          </aside>
+        </section>
+        <section className='w-full max-w-[1000px] flex flex-col gap-4 md:gap-6 md:pr-[5px]'>
+          <CodeBox />
+        </section>
       </main>
       <Footer />
     </>
